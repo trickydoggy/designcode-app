0fffcdb681b1cd2636d86073b9109e7bea48a075
trickyd <trickyeds@gmail.com>
2025-12-27 19:59:23 -0300

    graphql cards update

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -227,7 +227,7 @@ const Message = styled.Text`
 `;
 
 const CardsContainer = styled.View`
-    
+    flex-direction: row;
 `;
 
 
@@ -299,37 +299,38 @@ const logos = [
         text: "Sketch"
     },
 ];
-
+/*
 const cards = [
-    {
-        title: "React Native for Designers",
-        image: require('../assets/background11.jpg'),
-        subtitle: "React Native",
-        caption: "1 of 12 sections",
-        logo: require('../assets/logo-react.png'),
-    },
-    {
-        title: "Styled Components",
-        image: require('../assets/background12.jpg'),
-        subtitle: "React Native",
-        caption: "2 of 12 sections",
-        logo: require('../assets/logo-react.png'),
-    },
-    {
-        title: "Props and Icons",
-        image: require('../assets/background13.jpg'),
-        subtitle: "React Native",
-        caption: "3 of 12 sections",
-        logo: require('../assets/logo-react.png'),
-    },
-    {
-        title: "Static Data and Loop",
-        image: require('../assets/background14.jpg'),
-        subtitle: "React Native",
-        caption: "4 of 12 sections",
-        logo: require('../assets/logo-react.png'),
-    },
+   {
+       title: "React Native for Designers",
+       image: require('../assets/background11.jpg'),
+       subtitle: "React Native",
+       caption: "1 of 12 sections",
+       logo: require('../assets/logo-react.png'),
+   },
+   {
+       title: "Styled Components",
+       image: require('../assets/background12.jpg'),
+       subtitle: "React Native",
+       caption: "2 of 12 sections",
+       logo: require('../assets/logo-react.png'),
+   },
+   {
+       title: "Props and Icons",
+       image: require('../assets/background13.jpg'),
+       subtitle: "React Native",
+       caption: "3 of 12 sections",
+       logo: require('../assets/logo-react.png'),
+   },
+   {
+       title: "Static Data and Loop",
+       image: require('../assets/background14.jpg'),
+       subtitle: "React Native",
+       caption: "4 of 12 sections",
+       logo: require('../assets/logo-react.png'),
+   },
 ]
+*/
 
 const courses = [
     {
