e19fcb011ad331dac32f06d434f2cd8556e6ced1
trickyd <trickyeds@gmail.com>
2025-12-26 22:10:45 -0300

    HTML and Markdown chapter

--- screens/SectionScreen.js
+++ screens/SectionScreen.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components/native";
 import { TouchableOpacity, StatusBar } from "react-native";
+import { WebView, Linking } from "react-native-webview";
 import Ionicons from '@expo/vector-icons/Ionicons'
 
 class SectionScreen extends React.Component {
@@ -38,12 +39,55 @@ class SectionScreen extends React.Component {
                         <Ionicons name="close-outline" size={36} color="blue" style={{ marginTop: -2, marginLeft: -2 }} />
                     </CloseView>
                 </TouchableOpacity>
+                <Content>
+                    <WebView source={{ html: htmlContent + htmlStyle }}
+                        scalesPageToFit={false}
+                        scrollEnabled={false}
+                        onNavigationStateChange={event => {
+                            console.log(event.url);
+
+                            if (event.url != "about:blank") {
+                                this.refs.webview.stopLoading();
+                                Linking.openURL(event.url);
+                            }
+                        }}
+
+                    />
+                </Content>
             </Container>
         );
     }
 }
 
 export default SectionScreen;
+
+
+const htmlContent = `
+ <h2>This is a title</h2>
+ <p>This <strong>is</strong> a <a href="http://designcode.io">link</a></p>
+ <img src="https://cl.ly/8861f359ed6d/download/Wave14.jpg" />
+`;
+
+
+const htmlStyle = `
+    <style>
+        * {
+            font-family: -apple-system, Roboto;
+            margin: 0;
+            padding: 0;
+        }
+        img {
+            width: 100%;
+            border-radius: 10px;
+            margin-top: 20px;
+        }
+    </style>        
+`;
+
+const Content = styled.View`
+    height: 100%;
+    padding: 20px;
+`
 
 const Container = styled.View`
     flex: 1;
