0951cf80e7a48855389a929edf509b3e13e51290
trickyd <trickyeds@gmail.com>
2026-07-02 16:21:24 -0300

    correct app.json

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ import { registerRootComponent } from 'expo';
 // Disable Apollo Client deprecation warnings (e.g. canonizeResults)
 global[Symbol.for("apollo.deprecations")] = true;
 
+import { LogBox } from 'react-native';
+LogBox.ignoreLogs([
+  "Passing an object as the argument to 'navigate' is deprecated",
+]);
+
 import App from './App';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
